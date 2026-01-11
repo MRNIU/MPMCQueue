27f5bf75e0d09b1cb0c0d7af9729d14c8e27d044
Niu Zhihong <zhihong@nzhnb.com>
2026-01-11 15:57:21 +0800

    test: update
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- test/test.cpp
+++ test/test.cpp
@@ -220,3 +220,46 @@ TEST(MPMCQueueTest, FewProducersManyConsumers) {
 
     EXPECT_EQ(consumer_sum, total_items);
 }
+
+TEST(MPMCQueueTest, EightProducersEightConsumers) {
+    MPMCQueue<int, 4096> queue;
+    std::atomic<long long> consumer_sum{0};
+    const int num_producers = 8;
+    const int num_consumers = 8;
+    const int ops_per_producer = 50000;
+    
+    // Total items produced = num_producers * ops_per_producer
+    // Each consumer must consume = (Total items) / num_consumers
+    const int total_items = num_producers * ops_per_producer;
+    const int ops_per_consumer = total_items / num_consumers;
+
+    auto producer = [&]() {
+        for (int i = 0; i < ops_per_producer; ++i) {
+            while (!queue.push(1)) {
+                std::this_thread::yield();
+            }
+        }
+    };
+
+    auto consumer = [&]() {
+        int val;
+        for (int i = 0; i < ops_per_consumer; ++i) {
+            while (!queue.pop(val)) {
+                std::this_thread::yield();
+            }
+            consumer_sum += val;
+        }
+    };
+
+    std::vector<std::thread> producers;
+    std::vector<std::thread> consumers;
+
+    for (int i = 0; i < num_producers; ++i) producers.emplace_back(producer);
+    for (int i = 0; i < num_consumers; ++i) consumers.emplace_back(consumer);
+
+    for (auto& t : producers) t.join();
+    for (auto& t : consumers) t.join();
+
+    EXPECT_EQ(consumer_sum, total_items);
+}
+
